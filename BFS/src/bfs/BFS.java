857a70ba6d954388ec5ae34e1b714592bfcb051b
Ernesto Baltasar Nve Obono <nveobono1@gmail.com>
2019-03-18 17:33:23 +0100

    Update BFS.java

--- BFS/src/bfs/BFS.java
+++ BFS/src/bfs/BFS.java
@@ -5,8 +5,11 @@
  */
 package bfs;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -21,7 +24,9 @@ public class BFS {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         try{
+            
             System.out.println("Introduce el numero de nodos, aristas y nodo de inicio recorrido: ");
             int[] datos = new int[3];
             for(int i = 0; i < datos.length; i++){
@@ -29,11 +34,52 @@ public class BFS {
             }    
             System.out.println(Arrays.toString(datos));
             
+            List<Integer> nodoAristas = new ArrayList<>();
+            for(int i = 0; i < 2*datos[0]; i++){
+                int nodoAris = sc.nextInt();
+                nodoAristas.add(nodoAris);
+            }
+            
+            nodoAristas.forEach((out) -> {
+                System.out.println("ArrayList: " + nodoAristas.get(out));
+            });
+                       
+                       
         }catch(Exception e){
            System.out.println(e);
         }finally{
-            System.out.checkError();
+            System.out.println("Final");
         }
     }
     
+    
+    public static List<Integer> breadthFirstSearch(List<Integer>[] g, int v) {
+        int n = g.length;
+        List<Integer> traversal = new ArrayList<>(n);
+        boolean[] visited = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        visited[v] = true;
+        traversal.add(v);
+        q.add(v);
+        while (!q.isEmpty()) {
+            int aux = q.remove();
+            for (int adj : g[aux]) {
+                if (!visited[adj]) {
+                    visited[adj] = true;
+                    traversal.add(adj);
+                    q.add(adj);
+                }
+            }
+        }
+        return traversal;
+    }
+
+    public static List<Integer>[] breadthFirstSearch(List<Integer>[] g) {
+        int n = g.length;
+        List<Integer> traversals[] = new List[n];
+        for (int v = 0; v < n; v++) {
+            traversals[v] = breadthFirstSearch(g, v);
+        }
+        return traversals;
+    }
 }
