e2fbf74dd842247f50554a38b7262ac23378a0f2
Ernesto Baltasar Nve Obono <nveobono1@gmail.com>
2019-03-20 20:24:48 +0100

    Update Mergesort.java

--- mergesort/src/mergesort/Mergesort.java
+++ mergesort/src/mergesort/Mergesort.java
@@ -14,61 +14,123 @@ import java.util.Scanner;
  */
 public class Mergesort {
 
-    public static void main(String[] args) {
-        Scanner dimension = new Scanner(System.in);
-        int n = dimension.nextInt();
-        
-        int[] array = new int[n];
-        for(int i = 0; i < n; i++){
-            array[i] = dimension.nextInt();
+      // Helper method to print out the integer array.
+      private static void printArray(int[] array) {
+
+        for (int i : array) {
+            System.out.print(i + " ");
         }
-        mergeSort(array, 0, array.length - 1);
-        
-        for(int i = 0 ; i < array.length; i++){
-            System.out.print(array[i]+ " ");
-        }
+        System.out.println();
 
     }
-    
-    public static void mergeSort(int[] array, int inferior, int superior){
-        if(inferior < superior){
-            int medio = (superior + inferior)/2;
-            mergeSort(array, inferior, medio);
-            mergeSort(array, medio + 1, superior);
-            combinar(array, inferior, medio, superior);
+
+    // Breaks down the array to single or null elements in array.
+    private static int[] mergeSort(int[] array) {
+
+        // Recursive control 'if' statement.
+        if (array.length <= 1) {
+
+            return array;
+
         }
+
+        int midpoint = array.length / 2;
+
+        // Declare and initialize left and right arrays.
+        int[] left = new int[midpoint];
+        int[] right;
+
+        if (array.length % 2 == 0) { // if array.length is an even number.
+
+            right = new int[midpoint];
+
+        } else {
+
+            right = new int[midpoint + 1];
+
+        }
+
+        // Populate the left and right arrays.
+        for (int i = 0; i < midpoint; i++) {
+
+            left[i] = array[i];
+
+        }
+
+        for (int j = 0; j < right.length; j++) {
+
+            right[j] = array[midpoint + j];
+
+        }
+
+        int[] result = new int[array.length];
+
+        // Recursive call for left and right arrays.
+        left = mergeSort(left);
+        right = mergeSort(right);
+
+        // Get the merged left and right arrays.
+        result = merge(left, right);
+
+        // Return the sorted merged array.
+        return result;
+
     }
-    
-    public static void combinar(int[] array, int inf, int medio ,int sup){
-        int[] arrayAux = new int[array.length];
-        int izqInf = inf;
-        int izqSup = medio + 1;
-        int supAux = inf;
-        
-        while ((izqInf <= medio) && (izqSup <= sup)) {
-            if(array[izqInf] < array[izqSup]){
-                arrayAux[supAux] = array[izqInf];
-                izqInf++;
-            }else{
-                arrayAux[supAux] = array[izqSup];
-                izqSup++;
+
+    // Merges the left and right array in ascending order.
+    private static int[] merge(int[] left, int[] right) {
+
+        // Merged result array.
+        int[] result = new int[left.length + right.length];
+
+        // Declare and initialize pointers for all arrays.
+        int leftPointer, rightPointer, resultPointer;
+        leftPointer = rightPointer = resultPointer = 0;
+
+        // While there are items in either array...
+        while (leftPointer < left.length || rightPointer < right.length) {
+
+            // If there are items in BOTH arrays...
+            if (leftPointer < left.length && rightPointer < right.length) {
+
+                // If left item is less than right item...
+                if (left[leftPointer] < right[rightPointer]) {
+
+                    result[resultPointer++] = left[leftPointer++];
+
+                } else {
+
+                    result[resultPointer++] = right[rightPointer++];
+
+                }
+
+            } // If there are only items in the left array...
+            else if (leftPointer < left.length) {
+
+                result[resultPointer++] = left[leftPointer++];
+
+            } // If there are only items in the right array...
+            else if (rightPointer < right.length) {
+
+                result[resultPointer++] = right[rightPointer++];
+
             }
-            supAux++;
+
         }
-        
-        for(int i = izqInf; i <= medio; i++){
-            arrayAux[supAux] = array[i];
-            supAux++;
-        }
-        
-        for(int i = izqSup; i <= sup; i++){
-            arrayAux[supAux] = array[i];
-            supAux++;
-        }
-                
-        for(int i = inf; i <= sup; i++){
-            array[i] = arrayAux[i];
-        }               
+
+        return result;
+
     }
-    
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int[] arrayOrdenar = new int[num];
+        for (int i = 0; i < arrayOrdenar.length; i++) {
+            int numero = scanner.nextInt();
+            arrayOrdenar[i] = numero;
+        }
+        arrayOrdenar = mergeSort(arrayOrdenar);
+        printArray(arrayOrdenar);
+    }
 }
