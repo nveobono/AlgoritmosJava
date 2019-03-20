0d4f915f68f5aa2d267203bf13646bf4af30492f
Ernesto Baltasar Nve Obono <nveobono1@gmail.com>
2019-03-20 18:04:42 +0100

    Update BFS.java

--- BFS/src/bfs/BFS.java
+++ BFS/src/bfs/BFS.java
@@ -11,7 +11,11 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 /**
  *
  * @author nveob
@@ -22,66 +26,66 @@ public class BFS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner scanner = new Scanner(System.in);
+
         Scanner sc = new Scanner(System.in);
-        try{
-            
-            
-            List<Integer> g = new ArrayList<>();
-            g.add(1,2);
-            g.add(1,5);
-            g.add(2,3);
-            g.add(3,4);
-            g.add(3,5);
-            
-            BFS.breadthFirstSearch(g, 2);
-            
-            /*System.out.println("Introduce el numero de nodos, aristas y nodo de inicio recorrido: ");
-            int[] datos = new int[3];
-            for(int i = 0; i < datos.length; i++){
-                datos[i] = scanner.nextInt();
-            }    
-            System.out.println(Arrays.toString(datos));
-            
-            List<Integer> g = new ArrayList<Integer>();
-            for(int i = 0; i < 2*datos[0]; i++){
-                int nodoAris = sc.nextInt();
-                g.add(nodoAris);
-            }
-            
-            g.forEach((out) -> {
-                System.out.println("ArrayList: " + g.get(out));
-            });*/
-                       
-            
-        }catch(Exception e){
-           System.out.println(e);
-        }finally{
-            System.out.println("Final");
+        String strFirstLine = sc.nextLine();
+
+        //11 20 897
+        String[] items = strFirstLine.split(" ");
+        int n = Integer.parseInt(items[0]);
+        int m = Integer.parseInt(items[1]);
+        int s = Integer.parseInt(items[2]);
+
+        List<Integer>[] grafo = new List[n+1];
+
+        for (int i = 1; i<=m; i++)
+        {
+            grafo[i] = new ArrayList<>(n);
         }
+
+        for (int i = 1; i<=m; i++)
+        {
+            String readLine = sc.nextLine();
+            String[] elementos = readLine.split(" ");
+
+            int u = Integer.parseInt(elementos[0]);
+            int v = Integer.parseInt(elementos[1]);
+
+            grafo[u].add(v);
+            grafo[v].add(u);
+
+        }
+
+        List<Integer> recorrido = breadthFirstSearch(grafo, s);
+
+
+        for (int i = 0; i<recorrido.size(); i++)
+        {
+            System.out.print(recorrido.get(i) + " ");
+        }
+
+
     }
-    
-    
+
     public static List<Integer> breadthFirstSearch(List<Integer>[] g, int v) {
+
         int n = g.length;
         List<Integer> traversal = new ArrayList<>(n);
         boolean[] visited = new boolean[n];
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> cola = new LinkedList<>();
         visited[v] = true;
         traversal.add(v);
-        q.add(v);
-        while (!q.isEmpty()) {
-            int aux = q.remove();
-            g[aux].stream().filter((adj) -> (!visited[adj])).map((adj) -> {
-                visited[adj] = true;
-                return adj;
-            }).map((adj) -> {
-                traversal.add(adj);
-                return adj;
-            }).forEachOrdered((adj) -> {
-                q.add(adj);
-            });
+        cola.add(v);
+
+        while (!cola.isEmpty()) {
+            int aux = cola.remove();
+            for (int adj : g[aux]) {
+                if (!visited[adj]) {
+                    visited[adj] = true;
+                    traversal.add(adj);
+                    cola.add(adj);
+                }
+            }
         }
         return traversal;
     }
